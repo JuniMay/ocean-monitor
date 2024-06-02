e853b53d231db7b27749829dbea8158586aa3c12
suhipek <suhipek@pm.me>
2024-06-02 21:29:33 +0800

    chore: use higher ports to avoid collision

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,7 +8,9 @@ import { Provider } from "react-redux";
 import axios from "axios";
 
 
-axios.defaults.baseURL = "http://127.0.0.1:5000"
+// Set the base URL from the environment variable or use a default value
+const defaultBaseURL = "http://127.0.0.1:5000";
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL || defaultBaseURL;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
